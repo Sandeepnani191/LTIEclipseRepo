7e6dafc520fda2911d2fea7494c6576320fec06a
unknown <user224@ALCHEMYCLOUD.CO.IN>
2022-05-25 17:58:12 +0530

    First  commit is done

---
+++ src/com/lt/application/DemoApp.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package com.lt.application;
+
+/**
+ * @author user224
+ *
+ */
+public class DemoApp {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stu
+		System.out.println("This is my first java apllication");
+
+	}
+
+}
